fe7b4fa967df151a91d46de598a2b39041858caf
niceriver <foresetniceriver@outlook.com>
2026-05-27 17:33:06 +0800

    Add mini.sessions

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -17,4 +17,83 @@ return {
     version = false,
     event = "InsertEnter",
   },
+  {
+    "echasnovski/mini.sessions",
+    version = false,
+    event = "VeryLazy",
+    opts = {
+      autoread = true,
+      autowrite = true,
+
+      directory = vim.fn.stdpath("data") .. "/sessions",
+
+      file = "",
+
+      force = {
+        read = true,
+        write = true,
+        delete = false,
+      },
+
+      verbose = {
+        read = true,
+        write = true,
+        delete = true,
+      },
+    },
+    config = function(_, opts)
+      vim.o.sessionoptions = table.concat({
+        "buffers",
+        "curdir",
+        "folds",
+        "help",
+        "tabpages",
+        "winsize",
+        "terminal",
+        "localoptions",
+      }, ",")
+
+      require("mini.sessions").setup(opts)
+    end,
+
+    keys = {
+      {
+        "<leader>ps",
+        function()
+          require("mini.sessions").select()
+        end,
+        desc = "Select session",
+      },
+      {
+        "<leader>pw",
+        function()
+          local default = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+          local name = vim.fn.input("Session name: ", default)
+          if name == "" then
+            return
+          end
+          require("mini.sessions").write(name)
+        end,
+        desc = "Write session",
+      },
+      {
+        "<leader>pr",
+        function()
+          require("mini.sessions").select()
+        end,
+        desc = "Read session",
+      },
+      {
+        "<leader>pd",
+        function()
+          local name = vim.fn.input("Delete session name: ")
+          if name == "" then
+            return
+          end
+          require("mini.sessions").delete(name)
+        end,
+        desc = "Delete session",
+      },
+    },
+  },
 }
