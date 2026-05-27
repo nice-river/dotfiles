05bed817bcce676aaaa16e780c877cfa76fbb0ce
niceriver <foresetniceriver@outlook.com>
2026-05-27 17:39:30 +0800

    alias ls='eza'

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,4 +2,7 @@ if status is-interactive
 # Commands to run in interactive sessions can go here
 end
 
+alias ls='eza -lah --icons'
+alias ll='eza -lah --icons'
+alias la='eza -a --icons'
 starship init fish | source
