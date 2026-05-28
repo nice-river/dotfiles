d26921b0dd2807e1f3d1c67d9b9eee6c8bb6f3c5
nice-river <forestniceriver@outlook.com>
2026-05-29 00:48:54 +0800

    Fish config in MacOS

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
 # Commands to run in interactive sessions can go here
 end
 
+if test (uname) = Darwin
+  /opt/homebrew/bin/brew shellenv | source
+end
+
 alias ls='eza -lh --icons'
 alias ll='eza -lh --icons'
 alias la='eza -lah --icons'
