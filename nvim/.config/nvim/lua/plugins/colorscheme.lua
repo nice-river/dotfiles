d6b2746441f5a80a8b48eb364b333e7a02ca258e
nice-river <forestniceriver@outlook.com>
2026-05-30 23:30:00 +0800

    Ignore nvim lazy-lock.json

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,30 @@
 return { -- the colorscheme should be available when starting Neovim
   {
     "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function(_, opts)
-      -- load the colorscheme here
-      require("tokyonight").setup(opts)
-      vim.cmd.colorscheme("tokyonight")
+    lazy = true, -- make sure we load this during startup if it is your main colorscheme
+    -- priority = 1000, -- make sure to load this before all the other start plugins
+    -- config = function(_, opts)
+    --   -- load the colorscheme here
+    --   require("tokyonight").setup(opts)
+    --   vim.cmd.colorscheme("tokyonight")
+    -- end,
+  },
+  {
+    "thesimonho/kanagawa-paper.nvim",
+    lazy = true,
+    -- priority = 1000,
+    opts = {},
+  },
+  -- Using lazy.nvim
+  {
+    "ribru17/bamboo.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("bamboo").setup({
+        -- optional configuration here
+      })
+      vim.cmd.colorscheme("bamboo")
     end,
   },
   {
